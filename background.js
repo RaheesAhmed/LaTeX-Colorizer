3dc9ea35e0b1ea155918406285f6ab5884cf9881
Rahees Ahmed <raheesahmed256@gmail.com>
2024-12-03 06:03:20 +0500

    docs: standardize README filename to uppercase

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Background service worker for Wikipedia Math Formula Enhancer
 
 // Listen for extension installation or update
@@ -25,23 +24,3 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     return true; // Will respond asynchronously
   }
 });
-=======
-// Create context menu
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: "colorLatex",
-    title: "Color LaTeX",
-    contexts: ["selection"],
-  });
-});
-
-// Handle context menu clicks
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "colorLatex") {
-    chrome.tabs.sendMessage(tab.id, {
-      action: "colorSelection",
-      text: info.selectionText,
-    });
-  }
-});
->>>>>>> origin/main
